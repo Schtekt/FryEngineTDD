c5e24c1a6fcce3043d84cf0cb5dd22da2ad893fc
Tim Johansson <totimom@live.se>
2022-07-15 10:45:54 +0200

    Add clean and json task generation through premake

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,15 @@
+require("vscode")
+require("clean")
 workspace "FryWork"
+    toolset "msc"
     architecture "x86_64"
     cppdialect "C++17"
-    configurations
-    {
-        "Debug",
-        "Release"
-    }
+
+    configurations{"Debug", "Release"}
+
+filter "system:windows"
+    platforms {"Win32","Win64"}
+
 include "FryEngine"
 include "FryBox"
 include "FryTest"
